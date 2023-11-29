4489fd2578c1ffbc62d60b3fe8f6d611f11da7ce
surajkrgravitonweb <Suraj.kumar@gravitonweb.com>
2023-11-29 13:35:08 +0530

    corsSolve

--- joyoMoneyLoan/admin.py
+++ joyoMoneyLoan/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+admin.site.register(NormalData)
+admin.site.register(LoanApplication)
+admin.site.register(Email)
+admin.site.register(Image)
+admin.site.register(Register)
